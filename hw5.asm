ab409f2e7313ec310c97d7aba9440939c97a0b5e
Sidhya Jeyaram <163345433+sidhya10@users.noreply.github.com>
2024-11-21 23:33:29 +0000

    no message

--- hw5.asm
+++ hw5.asm
@@ -112,10 +112,8 @@ zero_col_loop:
     jr $ra
 
 place_tile:
-    addi $sp, $sp, -12
-    sw $ra, 8($sp)
-    sw $s0, 4($sp)
-    sw $s1, 0($sp)
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
 
     # Load board dimensions
     lw $t0, board_width
@@ -135,27 +133,23 @@ place_tile:
     la $t2, board         # board base address
     add $t2, $t2, $t1     # board + offset
     
-    # Check if occupied
-    lb $t4, 0($t2)
-    bnez $t4, occupied    # if cell != 0, occupied
+    # Check if occupied first before placing
+    lb $t4, 0($t2)       # Load current value
+    beqz $t4, place_val  # If zero, go place value
+    li $v0, 1            # occupied error
+    j place_tile_done
     
-    # Place tile
-    sb $a2, 0($t2)       # board[offset] = value
-    li $v0, 0            # return success
+place_val:
+    sb $a2, 0($t2)       # Set new value
+    li $v0, 0            # success
     j place_tile_done
 
 out_of_bounds:
-    li $v0, 2            # return out of bounds error
-    j place_tile_done
-
-occupied:
-    li $v0, 1            # return occupied error
+    li $v0, 2            # out of bounds error
 
 place_tile_done:
-    lw $ra, 8($sp)
-    lw $s0, 4($sp)
-    lw $s1, 0($sp)
-    addi $sp, $sp, 12
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
     jr $ra
 
 T_orientation4:
