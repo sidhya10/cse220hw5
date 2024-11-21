febdc8b6545743107366029d029ad950d3c80752
Sidhya Jeyaram <163345433+sidhya10@users.noreply.github.com>
2024-11-21 23:55:11 +0000

    no message

--- hw5.asm
+++ hw5.asm
@@ -220,41 +220,38 @@ placePieceOnBoard:
     blt $s3, $t0, invalid_piece
     bgt $s3, $t1, invalid_piece
     
-    # Try to place the anchor point
+    # Place anchor point
     move $a0, $s4
     move $a1, $s5
     move $a2, $s1
     jal place_tile
     
-    # If anchor placement failed, cleanup and return error
-    bnez $v0, cleanup_and_return
+    # If placement failed, handle error
+    bgtz $v0, handle_error
 
-    # Based on piece type and orientation, place remaining tiles
+    # Branch to piece handlers based on type
     li $t0, 1
     beq $s2, $t0, piece_square
-    li $t0, 2  
+    li $t0, 2
     beq $s2, $t0, piece_line
     li $t0, 3
     beq $s2, $t0, piece_reverse_z
     li $t0, 4
     beq $s2, $t0, piece_L
     li $t0, 5
-    beq $s2, $t0, piece_z  
+    beq $s2, $t0, piece_z
     li $t0, 6
     beq $s2, $t0, piece_reverse_L
-    
-    # Must be type 7 (T piece) since we validated earlier
     j piece_T
-    # Don't need piece_done here since piece handlers will jump there
 
 invalid_piece:
-    li $v0, 4          # Invalid type/orientation
+    li $v0, 4
     j piece_done
 
-cleanup_and_return:
-    move $t0, $v0      # Save error code
-    jal zeroOut        # Clear the board
-    move $v0, $t0      # Restore error code
+handle_error:
+    move $t0, $v0    # Save error code
+    jal zeroOut      # Clear the board
+    move $v0, $t0    # Restore error code
 
 piece_done:
     lw $ra, 24($sp)
@@ -266,7 +263,7 @@ piece_done:
     lw $s5, 0($sp)
     addi $sp, $sp, 28
     jr $ra
-
+    
 test_fit:
     addi $sp, $sp, -20
     sw $ra, 16($sp)
