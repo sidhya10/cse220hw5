a36b00a38dbbe92de017166615b230767bb223d8
Sidhya Jeyaram <163345433+sidhya10@users.noreply.github.com>
2024-11-22 23:09:14 +0000

    no message

--- hw5.asm
+++ hw5.asm
@@ -175,6 +175,9 @@ placePieceOnBoard:
     sw $ra, 4($sp)
     sw $s2, 0($sp)
     
+    # Clear board first to start fresh
+    jal zeroOut
+    
     # Load piece data into appropriate registers
     lw $t0, 0($a0)     # piece type
     lw $s4, 4($a0)     # orientation
@@ -192,10 +195,10 @@ placePieceOnBoard:
     li $t1, 4  
     bgt $s4, $t1, invalid_piece
     
-    # Initialize error accumulator before attempting placement
+    # Initialize error accumulator
     li $s2, 0          
     
-    # Try placing the piece
+    # Branch to appropriate piece handler in skeleton.asm
     li $t1, 1
     beq $t0, $t1, piece_square
     li $t1, 2
@@ -211,21 +214,19 @@ placePieceOnBoard:
     j piece_T
 
 invalid_piece:
-    jal zeroOut        # Clear board
-    li $v0, 2          # Return out of bounds error
+    li $v0, 2          # Out of bounds error for invalid type/orientation
     j piece_done
 
 piece_return:
     # If no errors, we're done
     beqz $s2, success
     
-    # If there were errors, clear board before returning
-    jal zeroOut
-    move $v0, $s2      # Return accumulated error code
+    # If there were errors, return error code (board already cleared)
+    move $v0, $s2
     j piece_done
 
 success:
-    li $v0, 0          # Return success
+    li $v0, 0
 
 piece_done:
     # Restore saved registers
