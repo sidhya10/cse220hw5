6bca326705cf27c14cc3cb680050211fae2d5371
Sidhya Jeyaram <163345433+sidhya10@users.noreply.github.com>
2024-11-22 22:54:06 +0000

    no message

--- hw5.asm
+++ hw5.asm
@@ -175,15 +175,12 @@ placePieceOnBoard:
     sw $ra, 4($sp)
     sw $s2, 0($sp)
     
-    # First zero out the board before attempting placement
-    jal zeroOut
-    
     # Load piece data into appropriate registers
+    lw $t0, 0($a0)     # piece type
     lw $s4, 4($a0)     # orientation
     lw $s5, 8($a0)     # row
     lw $s6, 12($a0)    # col
     move $s1, $a1      # ship_num
-    lw $t0, 0($a0)     # piece type
     
     # Validate type/orientation
     li $t1, 1
@@ -214,16 +211,20 @@ placePieceOnBoard:
     j piece_T
 
 invalid_piece:
-    li $v0, 4
+    # Clear the board and return error
+    jal zeroOut
+    li $v0, 2          # Out of bounds error for invalid type/orientation
     j piece_done
 
 piece_return:
     # If no errors, we're done
     beqz $s2, success
     
-    # Otherwise, clear board and return error
+    # If there were errors, clear board and return error code
     jal zeroOut
-    # Return accumulated error code directly
+    
+    # Return accumulated error code
+    # $s2 will be 1 for occupied, 2 for out of bounds, 3 for both
     move $v0, $s2
     j piece_done
 
