9993e6444207e48b4b4fcdd00ffd4b6fa7828191
Sidhya Jeyaram <163345433+sidhya10@users.noreply.github.com>
2024-11-25 19:07:42 +0000

    no message

--- hw5.asm
+++ hw5.asm
@@ -280,73 +280,72 @@ success:
     jr $ra
 
 test_fit:
-   addi $sp, $sp, -20
-   sw $ra, 16($sp)
-   sw $s0, 12($sp)
-   sw $s1, 8($sp) 
-   sw $s2, 4($sp)
-   sw $s3, 0($sp)
-   
-   move $s0, $a0      # piece array address
-   li $s1, 0          # piece counter 
-   li $s2, 0          # max error
+    addi $sp, $sp, -20
+    sw $ra, 16($sp)
+    sw $s0, 12($sp)
+    sw $s1, 8($sp) 
+    sw $s2, 4($sp)
+    sw $s3, 0($sp)
+    
+    move $s0, $a0      # piece array address
+    li $s1, 0          # piece counter 
+    li $s2, 0          # max error
 
-   jal zeroOut        # initial clear
+    jal zeroOut        # initial clear
 
 test_loop:
-   # Get current piece
-   li $t0, 16
-   mul $t0, $t0, $s1
-   add $s3, $s0, $t0
-   
-   # Validate piece
-   lw $t1, 0($s3)     
-   lw $t2, 4($s3)     
-   
-   li $t3, 1
-   blt $t1, $t3, invalid_fit_type
-   li $t3, 7
-   bgt $t1, $t3, invalid_fit_type
-   li $t3, 1
-   blt $t2, $t3, invalid_fit_type
-   li $t3, 4
-   bgt $t2, $t3, invalid_fit_type
+    # Get current piece
+    li $t0, 16
+    mul $t0, $t0, $s1
+    add $s3, $s0, $t0
+    
+    # Validate piece
+    lw $t1, 0($s3)     
+    lw $t2, 4($s3)     
+    
+    li $t3, 1
+    blt $t1, $t3, invalid_fit_type
+    li $t3, 7
+    bgt $t1, $t3, invalid_fit_type
+    li $t3, 1
+    blt $t2, $t3, invalid_fit_type
+    li $t3, 4
+    bgt $t2, $t3, invalid_fit_type
 
-   # Try piece
-   move $a0, $s3
-   addi $a1, $s1, 1
-   jal placePieceOnBoard
-   
-   beqz $v0, continue_fit_test
-   # Error occurred - save max error, clear board, start over
-   blt $v0, $s2, skip_update
-   move $s2, $v0
-skip_update:
-   jal zeroOut
-   li $s1, 0          # Reset to first piece
-   j test_loop        # Start over
+    # Try piece
+    move $a0, $s3
+    addi $a1, $s1, 1
+    jal placePieceOnBoard
+    
+    # Handle error case
+    bnez $v0, handle_error
+    j continue_fit_test
+    
+handle_error:
+    blt $v0, $s2, continue_fit_test  # Only update max error if larger
+    move $s2, $v0
 
 continue_fit_test:
-   addi $s1, $s1, 1
-   li $t0, 5          
-   bge $s1, $t0, test_done
-   j test_loop
+    addi $s1, $s1, 1
+    li $t0, 5          
+    bge $s1, $t0, test_done
+    j test_loop
 
 invalid_fit_type:
-   jal zeroOut
-   li $v0, 4
-   j test_fit_done
+    jal zeroOut
+    li $v0, 4
+    j test_fit_done
 
 test_done:
-   move $v0, $s2
+    move $v0, $s2
 
 test_fit_done:
-   lw $ra, 16($sp)
-   lw $s0, 12($sp)
-   lw $s1, 8($sp)
-   lw $s2, 4($sp)
-   lw $s3, 0($sp)
-   addi $sp, $sp, 20
-   jr $ra
+    lw $ra, 16($sp)
+    lw $s0, 12($sp)
+    lw $s1, 8($sp)
+    lw $s2, 4($sp)
+    lw $s3, 0($sp)
+    addi $sp, $sp, 20
+    jr $ra
 
 .include "skeleton.asm"
